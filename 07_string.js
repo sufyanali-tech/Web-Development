5fe8c3b44ff521ab86e697bf2eb697a3e8a1847d
Sufyan Ali <sufyan.dev@gmail.com>
2026-01-03 21:35:57 +0500

    update

---
+++ 07_string.js
@@ -0,0 +1,34 @@
+// Using latest trick to concatenate the strings in backticks ``
+
+let firsName = "Sufyan"
+let lastName = "Ali"
+
+console.log(`My Full name is : ${firsName} ${lastName}`);
+
+// Coverting the String to String object in JS
+
+const tempVariable = new String("object")
+console.log(typeof tempVariable);
+
+
+// Coverting the Number to Number object in JS
+
+const tempVariableForNumber = new Number(1234)
+console.log(typeof tempVariableForNumber);
+
+// Some String Method in JavaScript
+// Convert to UpperCase
+let myName = "sufyan ali"
+
+const result = myName.toUpperCase()
+console.log(result);
+
+// Covert to LowerCase
+
+let myFavouriteColor = "WHITE"
+
+let temp = myFavouriteColor.toLowerCase()
+console.log(temp);
+
+
+
